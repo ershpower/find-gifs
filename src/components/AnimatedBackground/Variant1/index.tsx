610e48df95c817f3fa9f603fa3792aa1ee73aba9
INNO\VDErshov <VDErshov@inno.tech>
2024-02-06 21:36:57 +0400

    fix

---
+++ src/components/AnimatedBackground/Variant1/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style.css';
+const Variant1 = () => {
+    return (
+        <div>
+            <div className="area">
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export { Variant1 };
